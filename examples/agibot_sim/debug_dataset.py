4d9afed996ed59db4dceced3ffbee5ec98d7dc60
Yangyang Fu <fuyy2008@gmail.com>
2025-07-31 09:52:19 -0500

    debugging dataset

--- examples/agibot_sim/debug_dataset.py
+++ examples/agibot_sim/debug_dataset.py
@@ -17,49 +17,6 @@ import openpi.training.data_loader as _data_loader
 import openpi.transforms as transforms
 
 
-
-
-
-class RemoveStrings(transforms.DataTransformFn):
-    def __call__(self, x: dict) -> dict:
-        return {k: v for k, v in x.items() if not np.issubdtype(np.asarray(v).dtype, np.str_)}
-
-
-def create_torch_dataloader(
-    data_config: _config.DataConfig,
-    action_horizon: int,
-    batch_size: int,
-    model_config: _model.BaseModelConfig,
-    max_frames: int | None = None,
-) -> tuple[_data_loader.Dataset, int]:
-    if data_config.repo_id is None:
-        raise ValueError("Data config must have a repo_id")
-    dataset = _data_loader.create_torch_dataset(data_config, action_horizon, model_config)
-    dataset = _data_loader.TransformedDataset(
-        dataset,
-        [
-            *data_config.repack_transforms.inputs,
-            *data_config.data_transforms.inputs,
-        ],
-    )
-    if max_frames is not None and max_frames < len(dataset):
-        num_batches = max_frames // batch_size
-        shuffle = True
-    else:
-        num_batches = len(dataset) // batch_size
-        shuffle = False
-    data_loader = _data_loader.TorchDataLoader(
-        dataset,
-        local_batch_size=batch_size,
-        num_workers=8,
-        shuffle=shuffle,
-        num_batches=num_batches,
-    )
-    return data_loader, num_batches
-
-
-
-
 def main(config_name: str='pi0_agibot_sim_lora', max_frames: int | None = None):
     config = _config.get_config(config_name)
     data_config = config.data.create(config.assets_dirs, config.model)
@@ -69,15 +26,19 @@ def main(config_name: str='pi0_agibot_sim_lora', max_frames: int | None = None):
         model_config=config.model,
         action_horizon=config.model.action_horizon,
         batch_size=config.batch_size,
-        num_batches=10
+        shuffle=False,
     )
     
-
+    tot_frames = len(data_loader._data_loader._data_loader.dataset)
+    print(f"Total frames in dataset: {tot_frames}")
+    tot_steps = tot_frames // config.batch_size + 1
     
-    for batch in tqdm.tqdm(data_loader, desc="Computing normalization stats", total=10):
-        print(batch)
-        print(batch['prompt'])
-
+    # Create the iterator once outside the loop
+    data_iter = iter(data_loader)
     
+    for i in tqdm.tqdm(range(tot_steps), desc="Loading dataset", total=tot_steps):
+        batch = next(data_iter)
+
+
 if __name__ == "__main__":
     tyro.cli(main)
--- examples/agibot_sim/test_lerobot_multdataset.py
+++ examples/agibot_sim/test_lerobot_multdataset.py
@@ -3,14 +3,19 @@ from lerobot.datasets.lerobot_dataset import MultiLeRobotDataset
 
 ds = MultiLeRobotDataset(
     repo_ids=[
+        "bot-pi/agibot-sim-clear-table-in-the-restaurant",
+        "bot-pi/agibot-sim-clear-the-countertop-waste",
+        "bot-pi/agibot-sim-heat-the-food-in-the-microwave",
+        "bot-pi/agibot-sim-make-a-sandwich",
+        "bot-pi/agibot-sim-open-drawer-and-store-items",
+        "bot-pi/agibot-sim-pack-in-the-supermarket",
+        "bot-pi/agibot-sim-pack-moving-objects-from-conveyor",
+        "bot-pi/agibot-sim-pickup-items-from-the-freezer",
         "bot-pi/agibot-sim-restock-supermarket-items", 
         "bot-pi/agibot-sim-stamp-the-seal"
     ],
-    root='dataset'
+    root='test'
 )
 
 
 print(f"Number of episodes: {len(ds)}")
-print(ds[0])
-
-print(ds[2000])
